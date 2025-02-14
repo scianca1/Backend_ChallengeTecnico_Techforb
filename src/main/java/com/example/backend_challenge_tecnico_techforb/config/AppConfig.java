fda9a59605fbe3ac499771ad834f93fdd4e53d63
Matias Sciancalepore <msciancalepore47@gmail.com>
2025-02-14 17:59:44 -0300

    agrego ruta cors publica del front

--- src/main/java/com/example/backend_challenge_tecnico_techforb/config/AppConfig.java
+++ src/main/java/com/example/backend_challenge_tecnico_techforb/config/AppConfig.java
@@ -58,7 +58,7 @@ public class AppConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins("https://challenge-dd31e.web.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowCredentials(true);
             }
